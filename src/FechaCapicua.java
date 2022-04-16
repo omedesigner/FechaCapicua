01f4fc118ca50df686957621f2ab13c15e73f641
ome_8 <ome_8@LAPTOP-2OAAT1A1>
2022-04-16 14:19:40 +0100

    Método principal y método esCapicua comentado

--- src/FechaCapicua.java
+++ src/FechaCapicua.java
@@ -2,7 +2,10 @@
 import java.util.Scanner;
 
 public class FechaCapicua {
-
+/**
+ * método principal
+ * @param args
+ */
 	public static void main(String[] args) {
 		
 		Scanner teclado = new Scanner(System.in);
